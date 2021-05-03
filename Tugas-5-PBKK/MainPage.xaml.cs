4dcc9854667c147e1f5678d61930478de98f2094
kaori02 <kaorigaming02@gmail.com>
2021-05-04 06:13:47 +0700

    Add project files.

---
+++ Tugas-5-PBKK/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tugas_5_PBKK
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"Tombol diklik {count} kali.";
+        }
+    }
+}
